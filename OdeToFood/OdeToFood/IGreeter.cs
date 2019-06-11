ddd3ff79ad5fd535adf4eb31346d32f040d305b4
Miguel Teheran <miguelteheran@gmail.com>
2019-06-11 12:58:36 -0500

    Injection, middleware and exceptions

---
+++ OdeToFood/OdeToFood/IGreeter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string MessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string MessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
--- OdeToFood/OdeToFood/Startup.cs
+++ OdeToFood/OdeToFood/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace OdeToFood
 {
@@ -16,19 +17,52 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, 
+                              IConfiguration configuration,
+                              IGreeter greeter,
+                              ILogger<Startup> logger)
         {
-            if (env.IsDevelopment())
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.Use(next =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                return async context =>
+                {
+                    logger.LogInformation("request is comming");
+
+                    if (context.Request.Path.StartsWithSegments("/mym"))
+                    {
+                        await context.Response.WriteAsync("Hit!!");
+                        logger.LogInformation("Request handled");
+                    }
+                    else
+                    {
+                        await next(context);
+                        logger.LogInformation("Response outgoing");
+                    }
+
+                };
+            });
+
+            app.UseWelcomePage(new WelcomePageOptions()
+            { 
+                Path="/wp"
+            
+            });
 
             app.Run(async (context) =>
             {
-                string greeting = configuration["Greeting"];
+                throw new Exception();
+
+                //string greeting = configuration["Greeting"];
+                string greeting = greeter.MessageOfTheDay();
                 await context.Response.WriteAsync(greeting);
             });
         }
