3f0a8b0d56d4e97c320eb7f3e8d54299a9b22e87
Miguel Teheran <miguelteheran@gmail.com>
2019-06-14 17:45:12 -0500

    adding viewcomponent example

---
+++ OdeToFood/OdeToFood/ViewComponents/GreeterViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreeterViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreeterViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.MessageOfTheDay();
+
+            return View("Default" , model);
+        }
+    }
+}
